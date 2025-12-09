03376204ccc1cccc4bb183b641aefde8a331f1ad
z125554-SUNGIL-BAE <z125554@sic.shibaura-it.ac.jp>
2025-12-09 21:07:01 +0900

    Update hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a[2][2];
+    int i, j;
+    double Determinant;
+    
+    for(i=0; i<2; i++)
+    {
+        for(j=0; j<2; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("You entered\n");
+    for(i=0; i<2; i++)
+    {
+        for(j=0; j<2; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    
+    Determinant = a[0][0]*a[1][1]-a[1][0]*a[0][1];
+    printf("Determinant is %f", Determinant);
+    return 0;
+}
 
