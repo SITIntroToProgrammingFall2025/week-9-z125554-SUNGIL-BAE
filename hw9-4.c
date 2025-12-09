db0a64fc4b918e8211112da4c3775360d934560c
z125554-SUNGIL-BAE <z125554@sic.shibaura-it.ac.jp>
2025-12-09 22:40:58 +0900

    Update hw9-4.c

--- hw9-4.c
+++ hw9-4.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int r, g, b;
+    char hex_digits[16] = {'0', '1', '2', '3', '4', '5', '6', '7', 
+                           '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+    printf("Enter R, G, B value\n");
+
+    scanf("%d %d %d", &r, &g, &b);
+
+    printf("\nThe hex code is #");
+
+    printf("%c", hex_digits[r / 16]);
+
+    printf("%c", hex_digits[r % 16]);
+
+    printf("%c", hex_digits[g / 16]);
+    printf("%c", hex_digits[g % 16]);
+
+    printf("%c", hex_digits[b / 16]);
+    printf("%c", hex_digits[b % 16]);
+
+    printf("\n");
+
+    return 0;
+}
 
