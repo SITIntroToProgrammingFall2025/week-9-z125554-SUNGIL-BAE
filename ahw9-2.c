500da9a6e32888aa0983f1c240b55bb5c4e4b142
z125554-SUNGIL-BAE <z125554@sic.shibaura-it.ac.jp>
2025-12-09 23:00:12 +0900

    Update ahw9-2.c

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int n = 5; // 배열의 크기를 저장할 변수
-    int arr[n];
+    int arr[5];
     int i, j, temp;
 
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < 5; i++) {
         scanf("%d", &arr[i]);
     }
 
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - 1 - i; j++) {
+    for (i = 0; i < 5 - 1; i++) {
+        for (j = 0; j < 5 - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
@@ -20,10 +19,11 @@ int main() {
     }
 
     printf("Sorted array:\n");
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < 5; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 
     return 0;
 }
+
