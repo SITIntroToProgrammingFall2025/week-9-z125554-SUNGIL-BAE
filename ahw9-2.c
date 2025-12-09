4d00686a0967c62b0b195e55ea2ee7feb84075e2
z125554-SUNGIL-BAE <z125554@sic.shibaura-it.ac.jp>
2025-12-09 23:05:16 +0900

    Update ahw9-2.c

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 int main() {
+    int n = 5;
     int arr[5];
     int i, j, temp;
-
-    for (i = 0; i < 5; i++) {
+    
+    printf("Input array:\n");
+    for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    for (i = 0; i < 5 - 1; i++) {
-        for (j = 0; j < 5 - 1 - i; j++) {
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
@@ -18,12 +20,13 @@ int main() {
         }
     }
 
-    printf("Sorted array:\n");
-    for (i = 0; i < 5; i++) {
+    printf("sorted array:\n");
+    for (i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 
     return 0;
 }
+
 
