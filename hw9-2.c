9dba21a4c6f14ee71f3892d4e4f14f7b5a09c06c
z125554-SUNGIL-BAE <z125554@sic.shibaura-it.ac.jp>
2025-12-09 21:13:39 +0900

    Update hw9-2.c

--- hw9-2.c
+++ hw9-2.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a[3][3];
+    int i, j;
+    
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("You entered\n");
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("Output\n");
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            printf("%d ", a[i][2-j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
